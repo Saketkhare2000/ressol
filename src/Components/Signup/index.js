e10a7d6ba213c386c4cd0bd4da6916cbd454fba5
Saket Khare <saketkhare2000@gmail.com>
2022-02-05 17:16:00 +0530

    alert added

--- src/Components/Signup/index.js
+++ src/Components/Signup/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./style.css";
-import mobile from "../../mobile.json";
 import { Link } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import { slideUp } from "../../Animation";
 import axios from "axios";
+import validator from "validator";
+import { WebContext } from "../../Context/WebContext";
 const SignUp = () => {
   //name state
   const [name, setName] = useState("");
@@ -19,6 +20,8 @@ const SignUp = () => {
   //mobile number
   const [mobileNumber, setMobileNumber] = useState(0);
 
+  const { setAlert } = useContext(WebContext);
+
   const userDetails = {
     name: name,
     email: email,
@@ -30,21 +33,151 @@ const SignUp = () => {
     address: "address 51",
     pincode: "51",
   };
-  const passwordCheck = () => {
-    if (password !== confirmPassword) {
+
+  //validator functions
+  const validateEmail = (email) => {
+    if (validator.isEmail(email)) {
+      setError("");
+    } else {
+      setError("Invalid Email");
+      setAlert({
+        show: true,
+        message: "Invalid Email",
+        type: "danger",
+      });
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        });
+      }, 2000);
+    }
+  };
+  const validateMobile = (mobile) => {
+    if (mobile.length !== 10) {
+      setError("Invalid Mobile Number");
+      setAlert({
+        show: true,
+        message: "Enter a valid mobile number",
+        type: "danger",
+      });
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        });
+      }, 2000);
+    } else {
+      setError("");
+    }
+  };
+  const validateName = (name) => {
+    setError("Enter a valid name");
+    if (name.length < 3) {
+      setAlert({
+        show: true,
+        message: "Enter a valid name",
+        type: "danger",
+      });
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        });
+      }, 2000);
+    } else {
+      setError("");
+    }
+  };
+  const validatePassword = (password) => {
+    setError("Enter a valid password");
+    if (password.length < 8) {
+      setAlert({
+        show: true,
+        message: "Password must be atleast 8 characters long",
+        type: "danger",
+      });
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        });
+      }, 2000);
+    } else if (password !== confirmPassword) {
       setError("Passwords do not match");
+      setAlert({
+        show: true,
+        message: "Passwords do not match",
+        type: "danger",
+      });
+
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        });
+      }, 2000);
     } else {
       setError("");
     }
   };
 
+  const validateForm = () => {
+    validateName(name);
+    validateEmail(email);
+    validatePassword(password);
+    validateMobile(mobileNumber);
+  };
+
   //submit function
-  const handleSignUp = async (e) => {
+  const handleSignUp = (e) => {
     e.preventDefault();
-    passwordCheck();
-    axios.post("http://127.0.0.1:8000/api/user/", userDetails).then((res) => {
-      console.log(res);
-    });
+    validateForm();
+    if (error === "") {
+      axios
+        .post("http://127.0.0.1:8000/api/user/", userDetails)
+        .then((res) => {
+          setTimeout(() => {
+            setAlert({
+              show: true,
+              message: "User created successfully",
+              type: "success",
+            });
+          }, 2000);
+        })
+        .catch((err) => {
+          setAlert({
+            show: true,
+            message: "User already exists",
+            type: "danger",
+          });
+          setTimeout(() => {
+            setAlert({
+              show: false,
+              message: "",
+              type: "",
+            });
+          }, 2000);
+        });
+    } else {
+      setAlert({
+        show: true,
+        message: error,
+        type: "danger",
+      });
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        });
+      }, 2000);
+    }
   };
   return (
     <AnimatePresence initial={true} exitBeforeEnter>
@@ -65,6 +198,7 @@ const SignUp = () => {
                   type="radio"
                   name="type"
                   placeholder="Buyer/Owner"
+                  required
                 />
                 <label htmlFor="Buyer">Buyer/Owner</label>
               </div>
@@ -75,6 +209,7 @@ const SignUp = () => {
                   name="type"
                   placeholder="Seller"
                   value="Builder"
+                  required
                 />
                 <label htmlFor="Builder">Builder</label>
               </div>
@@ -85,6 +220,7 @@ const SignUp = () => {
                   name="type"
                   placeholder="Agent"
                   value="Agent"
+                  required
                 />
                 <label htmlFor="Agent">Agent</label>
               </div>
@@ -95,6 +231,7 @@ const SignUp = () => {
                 id="Name"
                 type="text"
                 placeholder="Name"
+                required
               />
             </div>
             <div className="form-category">
@@ -103,6 +240,7 @@ const SignUp = () => {
                 id="Email"
                 type="text"
                 placeholder="Email"
+                required
               />
             </div>
             <div className="form-category">
@@ -111,6 +249,7 @@ const SignUp = () => {
                 id="Password"
                 type="password"
                 placeholder="Password"
+                required
               />
             </div>
             <div className="form-category">
@@ -119,6 +258,7 @@ const SignUp = () => {
                 id="ConfirmPassword"
                 type="password"
                 placeholder="Confirm Password"
+                required
               />
             </div>
             <div className="form-category">
@@ -126,12 +266,13 @@ const SignUp = () => {
                 onChange={(e) => setMobileNumber(e.target.value)}
                 type="text"
                 placeholder="Mobile No."
+                required
               />
             </div>
             <motion.button
               whileTap={{ scale: 0.9 }}
               whileHover={{ scale: 1.03 }}
-              onClick={handleSignUp}
+              onClick={(e) => handleSignUp(e)}
             >
               Sign Up
             </motion.button>
